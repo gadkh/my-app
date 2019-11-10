714ddbcc61962a88bdb4f24dc437f11b159fa120
Liad Khamdadash <liadkh95@gmail.com>
2019-11-10 13:08:02 +0200

    Create Header and Posts page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{PostCeateComponent} from './posts/post-create/post-create.component';
+import{HeaderComponent} from './header/header.component';
+
+import {MatInputModule} from '@angular/material/input';
+import{MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCeateComponent,
+    HeaderComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
