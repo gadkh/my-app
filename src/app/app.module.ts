b053475a2b9cab438bd2e6ec518927a3c4ee14bd
Gad <gadkh2@gmail.com>
2019-11-12 15:15:18 +0200

    Create Post Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import{PostCeateComponent} from './posts/post-create/post-create.component';
 import{HeaderComponent} from './header/header.component';
+import{PostListComponent} from './posts/post-list/post-list.component';
 
 import {MatInputModule} from '@angular/material/input';
 import{MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCeateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -27,7 +33,9 @@ import {MatButtonModule} from '@angular/material/button';
     FormsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
     
     
   ],
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
-import {Component}from '@angular/core';
+import {Component,EventEmitter,Output}from '@angular/core';
+import {Post} from '../post.model'
 
 @Component({
     selector:'app-post-create',
@@ -6,9 +7,15 @@ import {Component}from '@angular/core';
     styleUrls:['./post-create.component.css']
 })
 export class PostCeateComponent{
-    eneteredValue='';
-    newPost='NO CONTENT';
+    enteredTitle='';
+    eneteredContent='';
+    @Output() postCreated=new EventEmitter<Post>();
+
     onAddPost(){
-        this.newPost=this.eneteredValue;
+        const post : Post={
+            title: this.enteredTitle,
+            content: this.eneteredContent
+        };
+        this.postCreated.emit(post);
     }
 }
